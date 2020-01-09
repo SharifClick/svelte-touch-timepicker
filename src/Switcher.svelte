e4bf5f8677fe5f154f210a00128eb887f805c5bf
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-01-09 19:46:59 +0600

    main files

---
+++ src/Switcher.svelte
@@ -0,0 +1,144 @@
+<script>
+
+  import { afterUpdate, onMount, createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  export let selected;
+  export let data = 0;
+  export let type;
+
+  let position = selected ? -(selected - 1) * 50 : 0;
+  let offset = 0;
+  let dragging = false;
+
+  let itemWrapper, previousY;
+
+
+  onMount(() => {
+   setPosition()
+  });
+
+  afterUpdate(() => {
+		let selectedPosition = -(selected - 1) * 50
+
+    if (!dragging && position !== selectedPosition) {
+        position = selectedPosition
+        setPosition()
+    }
+  });
+
+
+  function onDateChange(type, changedData) {
+		dispatch('dateChange', {
+			type, changedData
+		});
+  }
+
+  function setPosition(){
+     let itemPosition = `
+      will-change: 'transform';
+      transition: transform ${Math.abs(offset) / 100 + 0.1}s;
+      transform: translateY(${position}px)
+    `;
+    itemWrapper.style.cssText = itemPosition;
+  }
+
+  let onMouseDown = (event) => {
+    previousY = event.touches ? event.touches[0].clientY : event.clientY;
+    dragging = true;
+
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+    window.addEventListener('touchmove', onMouseMove)
+    window.addEventListener('touchend', onMouseUp)
+  }
+
+   let onMouseMove = (event) => {
+    let clientY = event.touches ? event.touches[0].clientY : event.clientY;
+    offset = clientY - previousY;
+
+    let maxPosition = -data.length * 50;
+    let _position = position + offset;
+    position = Math.max(maxPosition, Math.min(50, _position))
+    previousY = event.touches ? event.touches[0].clientY : event.clientY;
+    setPosition();
+  }
+
+  let onMouseUp = () => {
+    let maxPosition = -(data.length - 1) * 50;
+    let rounderPosition = Math.round((position + offset * 5) / 50) * 50;
+    let finalPosition = Math.max(maxPosition, Math.min(0, rounderPosition));
+
+    dragging = false;
+    position = finalPosition;
+
+    window.removeEventListener('mousemove', onMouseMove)
+    window.removeEventListener('mouseup', onMouseUp)
+    window.removeEventListener('touchmove', onMouseMove)
+    window.removeEventListener('touchend', onMouseUp);
+
+    setPosition();
+    onDateChange(type, -finalPosition / 50);
+  }
+
+
+</script>
+
+
+<style>
+ .touch-time-wrapper {
+  position: relative;
+  height: 50px;
+  margin: 0 10px;
+  border-top: 1px solid var(--svtt-bar-color, grey);
+  border-bottom: 1px solid var(--svtt-bar-color, grey);
+  border-radius: 0;
+}
+.touch-time-container {
+  margin: 0;
+  padding: 0;
+}
+
+.touch-time-wrapper:before,
+.touch-time-wrapper:after {
+  content: '';
+  position: absolute;
+  left: 0;
+  width: 80px;
+  height: 50px;
+  background-color: #fff;
+  opacity: 0.8;
+  pointer-events: none;
+  z-index: 1;
+}
+
+.touch-time-wrapper:before {
+  top: -51px;
+}
+
+.touch-time-wrapper:after {
+  bottom: -51px;
+}
+
+.touch-time-container li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 50px;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+}
+</style>
+
+
+<div class='touch-time-wrapper' on:mousedown={onMouseDown} on:touchstart={onMouseDown}>
+  <ul bind:this={itemWrapper} class="touch-time-container">
+   {#each data as item }
+     <li>{item}</li>
+   {/each}
+  </ul>
+</div>
---
+++ src/TimePicker.svelte
@@ -0,0 +1,124 @@
+<script>
+  import Switcher from './Switcher.svelte';
+
+  const HOURS = new Array(12).fill(1).map((v, i) => v + i);
+  const MINUTES = new Array(59).fill(1).map((v, i) => v + i);
+  const MERIDIEM = ['AM', 'PM'];
+
+
+  export let time = new Date();
+  export let _time, selectedHour, selectedMeridiem;
+  export let visible = false;
+
+  let resetTime = (event) => {
+    event.stopPropagation()
+    time = new Date();
+  }
+
+  $: {
+    _time = time.toLocaleTimeString('en-US', {timeStyle: 'short'});
+    selectedHour = +time.toLocaleTimeString('en-us', {hour12:true, hour:'numeric'}).split(' ')[0];
+    selectedMeridiem = time.getHours() < 12 ? 1 : 2;
+  }
+
+  let timeChanged = (event) => {
+
+    let {type, changedData} = event.detail;
+    let newTime = new Date();
+
+    if (type === 'hours'){
+      let thresholdHour = selectedMeridiem === 2 ? 13 : 1;
+      newTime.setHours(changedData + thresholdHour);
+      newTime.setMinutes(time.getMinutes())
+
+    } else if (type === 'minutes'){
+
+      newTime.setHours(time.getHours())
+      newTime.setMinutes(changedData + 1)
+
+    } else if (type === 'meridiem'){
+      let thresholdHour = ~~changedData ? 12 : ~11;
+      newTime.setHours(time.getHours() + thresholdHour )
+      newTime.setMinutes(time.getMinutes())
+    }
+
+    time = newTime;
+  }
+</script>
+
+<style>
+.touch-time {
+  font-size: 30px;
+  font-weight: 300;
+}
+
+.touch-time-popup{
+ position: absolute;
+  top: 0;
+  height: 100vh;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.3);
+}
+.touch-time-popup > div{
+    background: white;
+    margin-top: 30vh;
+    width: 80%;
+    margin-left: 7%;
+    border-radius: 10px;
+    padding: 10px;
+}
+.touch-time-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font: 20px 'Roboto', sans-serif;
+}
+
+.touch-time-picker {
+  display: flex;
+  padding: 50px 20px;
+  margin: 30px 0;
+  overflow: hidden;
+}
+
+.touch-time-reset > button {
+  width: 100px;
+  height: 30px;
+  border-radius: 15px;
+  border: none;
+  outline: none;
+  color: #fff;
+  background-color: #2466fb;
+  box-shadow: 0 1px 10px -2px #2466fb;
+  font-weight: 300;
+}
+.touch-time-reset:active {
+  -webkit-transform: scale(0.95);
+          transform: scale(0.95);
+}
+
+</style>
+
+<input type="text" readonly value={_time} on:focus={() => {visible = !visible}}>
+{#if visible}
+  <div class="touch-time-popup" >
+    <div>
+      <div class="touch-time-wrapper">
+          <div class='touch-time'>{_time}</div>
+          <div class='touch-time-picker'>
+            <Switcher type='hours' data={HOURS} selected={selectedHour} on:timeChange={timeChanged} }/>
+            <Switcher type='minutes' data={MINUTES} selected={time.getMinutes() } on:timeChange={timeChanged}/>
+            <Switcher type='meridiem' data={MERIDIEM} selected={selectedMeridiem} on:timeChange={timeChanged}/>
+          </div>
+        <div class='touch-time-reset'>
+          <button on:click={resetTime}>Reset</button>
+          <button on:click={() => {visible = !visible}}>Ok</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+{/if}
+
